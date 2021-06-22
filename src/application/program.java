12b6092b3efa607c714e677eb35593bfaecce42f
Gustavo Goulart <gustavogmfarias@gmail.com>
2021-06-22 15:30:59 -0300

    Project created

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Department dp = new Department(1, "books");
+		System.out.println(dp);
+		
+	}
+
+}
